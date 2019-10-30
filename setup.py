7b4919d447b0deefe0576707b415db552dd8fdaa
Kyle Smith <ksmith@smc3.com>
2019-10-30 12:43:08 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os.path
+
+from setuptools import setup
+
+
+dir_ = os.path.abspath(os.path.dirname(__file__))
+# get the version to include in setup()
+with open(f'{dir_}/asynctailer/__init__.py') as fh:
+    for line in fh:
+        if '__version__' in line:
+            exec(line)
+
+
+setup(
+    name='asynctailer',
+    version=__version__,
+    license='MIT',
+    author='Kyle Smith',
+    author_email='smithk86@gmail.com',
+    url='https://github.com/smithk86/asynctailer',
+    packages=[
+        'asynctailer'
+    ],
+    install_requires=[
+        'tailer',
+        'janus'
+    ]
+)
