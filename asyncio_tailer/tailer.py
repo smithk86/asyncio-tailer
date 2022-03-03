637c322d297c9e828f00a92fc8d141799d8f6f6b
Kyle Smith <smithk86@gmail.com>
2022-03-02 20:00:38 -0500

    0.6.1

--- asyncio_tailer/tailer.py
+++ asyncio_tailer/tailer.py
@@ -1,14 +1,12 @@
 from __future__ import annotations
 
 import asyncio
-import functools
 import time
 from pathlib import Path
 
 from typing import TYPE_CHECKING
 
 from asyncio_generator_converter import asyncio_generator_converter
-from concurrent.futures import Executor
 from tailer import Tailer as BaseTailer
 
 
@@ -20,39 +18,14 @@ if TYPE_CHECKING:
 NoneType: Type = type(None)
 
 
-def run_in_executor(func):
-    """
-    This decorator is specifically for use with class methods and requires self.executor to be set to
-    either None or concurrent.futures.Executor. The method will be converted from sync to async and
-    executed in the given Executor.
-    """
-
-    @functools.wraps(func)
-    async def wrapper(self, *args, **kwargs):
-        if not hasattr(self, "_executor") or not isinstance(
-            self._executor, (NoneType, Executor)
-        ):
-            raise TypeError(
-                "@run_in_executor requires self._executor to be None or an instance of concurrent.futures.Executor"
-            )
-
-        _loop = asyncio.get_running_loop()
-        _func = functools.partial(func, self, *args, **kwargs)
-        return await _loop.run_in_executor(self._executor, _func)
-
-    return wrapper
-
-
 class Tailer:
     def __init__(
         self,
         file: TextIOBase,
         read_size: int = 1024,
         end: bool = False,
-        executor: Optional[Executor] = None,
     ):
         self._base_tailer: BaseTailer = BaseTailer(file, read_size=read_size, end=end)
-        self._executor: Optional[Executor] = executor
         self._follow_running: bool = False
 
     @property
@@ -65,13 +38,11 @@ class Tailer:
     def __getattr__(self, name: str) -> Any:
         return getattr(self._base_tailer, name)
 
-    @run_in_executor
-    def tail(self, lines: int = 10) -> List[str]:
-        return self._base_tailer.tail(lines=lines)
+    async def tail(self, lines: int = 10) -> List[str]:
+        return await asyncio.to_thread(self._base_tailer.tail, lines=lines)
 
-    @run_in_executor
-    def head(self, lines: int = 10) -> List[str]:
-        return self._base_tailer.head(lines=lines)
+    async def head(self, lines: int = 10) -> List[str]:
+        return await asyncio.to_thread(self._base_tailer.head, lines=lines)
 
     def stop_following(self) -> None:
         self._follow_running = False
